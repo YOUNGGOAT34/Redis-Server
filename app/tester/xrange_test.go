f54c04e463cd07b07d7530c308bffde6fbd918d3
GOAT <lucasonyiego283@gmail.com>
2026-07-03 20:42:04 +0300

    wrong arguments to xrange

--- app/tester/xrange_test.go
+++ app/tester/xrange_test.go
@@ -13,7 +13,7 @@ func xrange_test(t *testing.T) {
 	stage80_XRangeNoMatches(t)
 	stage81_XRangeMissingKey(t)
 	stage82_XRangeWrongType(t)
-	// stage83_XRangeWrongArguments(t)
+	stage83_XRangeWrongArguments(t)
 	// stage84_XRangeConcurrentReads(t)
 }
 
