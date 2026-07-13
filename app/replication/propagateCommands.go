f7a1552fd39599370615861e328d04c0a2c9e052
GOAT <lucasonyiego283@gmail.com>
2026-07-13 18:21:16 +0300

    Fixing a bug : replicas are not executing the propagated requests

--- app/replication/propagateCommands.go
+++ app/replication/propagateCommands.go
@@ -1,12 +1,17 @@
 package replication
 
-import "CacheDB/app/RESP"
+import (
+	"CacheDB/app/RESP"
+	"fmt"
+)
 
 func PropagateCommands(parsedRequest []byte,config *RESP.SERVER){
 	
 	  config.ReplicasMutex.Lock()
 	  defer config.ReplicasMutex.Unlock()
+
 	  for i:=0;i<len(config.REPLICAS);{
+		     fmt.Printf("%q\n",parsedRequest)
 		      _,err:=config.REPLICAS[i].Write(parsedRequest)
 
 				if err!=nil{
@@ -15,6 +20,7 @@ func PropagateCommands(parsedRequest []byte,config *RESP.SERVER){
 					  config.REPLICAS = append(config.REPLICAS[:i],config.REPLICAS[i+1:]...)
 					  continue
 				}
+
 				i++
 	  }
 
