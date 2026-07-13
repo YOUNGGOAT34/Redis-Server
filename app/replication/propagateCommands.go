fc6c658ad69f59c3154125e34ae816af64a8dc0c
GOAT <lucasonyiego283@gmail.com>
2026-07-13 18:03:04 +0300

    removing the closed replicas and closing their connection

--- app/replication/propagateCommands.go
+++ app/replication/propagateCommands.go
@@ -4,14 +4,16 @@ import "CacheDB/app/RESP"
 
 func PropagateCommands(parsedRequest []byte,config *RESP.SERVER){
 	
-	  for i,replica :=range config.REPLICAS{
-		      _,err:=replica.Write(parsedRequest)
+	  for i:=0;i<len(config.REPLICAS);{
+		      _,err:=config.REPLICAS[i].Write(parsedRequest)
 
 				if err!=nil{
 					  //if the write fails remove the replica
+					  config.REPLICAS[i].Close()
 					  config.REPLICAS = append(config.REPLICAS[:i],config.REPLICAS[i+1:]...)
 					  continue
 				}
+				i++
 	  }
 
 }
