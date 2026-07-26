c7bfcecca5292d46587996f3b532189adc46f81e
GOAT <lucasonyiego283@gmail.com>
2026-07-26 20:30:45 +0300

    publishing messages to subscribed clients

--- app/server/publisherSubscriber.go
+++ app/server/publisherSubscriber.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 )
 
-func encodePubSubResponse(channel []byte, count int) []byte {
+func encodeSubResponse(channel []byte, count int) []byte {
 	var resp []byte
 	resp = fmt.Appendf(resp, "*3\r\n")
 	resp = fmt.Appendf(resp, "$%d\r\nsubscribe\r\n", len("subscribe"))
@@ -24,26 +24,34 @@ func sub(serverConfig *config.SERVER,client *storage.Client, args [][]byte) RESP
 	}
 
 	serverConfig.PubSub.ChannelMutex.Lock()
-
-	for _,channel :=range args[1:]{
-		  
-		   if _,exists:=serverConfig.PubSub.Channels[string(channel)];!exists{
-				       serverConfig.PubSub.Channels[string(channel)]=storage.NewSet[net.Conn]()
-			}
-			serverConfig.PubSub.Channels[string(channel)].Add(client.Conn)
-			client.SubscribedChannels.Add(string(channel))  
-	}
-
-	serverConfig.PubSub.ChannelMutex.Unlock()
-
+   
 	if !client.InSubscribeMode{
 
 		client.InSubscribeMode=true
 	}
 
 
+   var response []byte
+
+	for _,channel :=range args[1:]{
+		    
+		   channelName:=string(channel)
+
+		   if _,exists:=serverConfig.PubSub.Channels[channelName];!exists{
+				       serverConfig.PubSub.Channels[channelName]=storage.NewSet[net.Conn]()
+			}
+			serverConfig.PubSub.Channels[channelName].Add(client.Conn)
+			client.SubscribedChannels.Add(channelName)
+			
+			response = append(response, encodeSubResponse(channel,client.SubscribedChannels.Len())...)
+	}
+
+	serverConfig.PubSub.ChannelMutex.Unlock()
+
+
+
 	return RESP.Response{
-		Body: encodePubSubResponse(args[1], client.SubscribedChannels.Len()),
+		Body: response,
 		Type: RESP.ARRAY,
 	}
 }
@@ -56,19 +64,36 @@ func pub(serverConfig *config.SERVER,args [][]byte,) RESP.Response{
 		 }
 
 	
+	//copy the subscribed clients to avoid holding the lock while sending messages
+	
    serverConfig.PubSub.ChannelMutex.RLock()
-	defer serverConfig.PubSub.ChannelMutex.RUnlock()
-	channel:=serverConfig.PubSub.Channels[string(args[0])]
+	subscribedclients:=make([]net.Conn,0,serverConfig.PubSub.Channels[string(args[0])].Len())
+
+	for client:=range serverConfig.PubSub.Channels[string(args[0])]{
+		    subscribedclients = append(subscribedclients, client)
+	} 
+	serverConfig.PubSub.ChannelMutex.RUnlock()
+
+   
+	 response:=RESP.EncodeArray([][]byte{[]byte("message"),args[0],args[1]})
+
+    for _,client:=range subscribedclients{
+		     _,err:=client.Write(response)
+
+			  if err!=nil{
+
+			  }
+	 }
+	
+
 	return RESP.Response{
-		      Body: []byte(strconv.FormatInt(int64(channel.Len()),10)),
+		      Body: []byte(strconv.FormatInt(int64(len(subscribedclients)),10)),
 				Type: RESP.INTEGER,
 	}
 		 
 }
 
 //determines whether a command is legal in subscribe mode
-
- 
 func isLegal(command string) bool{
 
 	  switch command{
