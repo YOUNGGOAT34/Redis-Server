89fa6eec220c4c80b1131319285c4b9115f4e99c
GOAT <lucasonyiego283@gmail.com>
2026-08-14 18:35:00 +0300

    listpack header

--- app/streams/listpack.go
+++ app/streams/listpack.go
@@ -1,5 +1,7 @@
 package streams
 
+import "encoding/binary"
+
 const(
 	  EOF byte=0xFF
 	  HeaderSize int =6
@@ -14,8 +16,27 @@ func NewListPack() *ListPack{
 		  data:make([]byte,HeaderSize+1),
 	 }
 
+	 //total size
+	 binary.LittleEndian.PutUint32(lp.data[0:4],uint32(len(lp.data)))
+	 //number of elements-->at this point there are 0 elemenents
+	 binary.LittleEndian.PutUint16(lp.data[4:6],0)
+	 //end of file
 	 lp.data[6]=EOF
 	 return lp
+}
+
+/*
+   Read the header:
+	    1.first 4 bytes-->total bytes
+		 2.next 2 bytes--->number of elements
+*/
+
+func(lp *ListPack) TotalBytes() uint32{
+	  return binary.LittleEndian.Uint32(lp.data[0:4])
+}
+
+func (lp *ListPack) Length()uint16{
+	  return binary.LittleEndian.Uint16(lp.data[4:6])
 }
 
 
